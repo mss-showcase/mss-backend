dd390b173da190f7b9b74901fb54d46cd928bd13
Malyik József <jozsef.malyik@barre.hu>
2025-07-05 14:48:49 +0200

    missing dependencies

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { CognitoIdentityProviderClient, AdminGetUserCommand, AdminUpdateUserAttributesCommand, ListUsersCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { CognitoIdentityProviderClient, AdminUpdateUserAttributesCommand, ListUsersCommand } from '@aws-sdk/client-cognito-identity-provider';
 
 const router = express.Router();
 
